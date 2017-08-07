ecd12cbff980b201725c4d2ce9a214cf8f5c0057
Michael Kong <michael.kong@yuceanalytics.com>
2017-08-07 14:32:49 +0930

    test: 0.0.3 + new js

--- inst/htmlwidgets/echarts.js
+++ inst/htmlwidgets/echarts.js
@@ -5,9 +5,11 @@ HTMLWidgets.widget({
   initialize: function(el, width, height) {
     return echarts.init(el);
   },
-
+	
   renderValue: function(el, x, instance) {
-    instance.setOption(x);
+    instance.setOption(x, true);
+	//instance.setTheme(eval(x.theme + "Theme"));
+
   },
 
   resize: function(el, width, height, instance) {
