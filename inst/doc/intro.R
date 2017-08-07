b5bf6f762f37a7bf63455a5646ef3f365f8f03f3
Michael Kong <michael.kong@yuceanalytics.com>
2017-08-07 13:31:57 +0930

    clean

--- inst/doc/intro.R
+++
@@ -1,6 +0,0 @@
-## ------------------------------------------------------------------------
-library(recharts)
-# you can also write: iris %>% echart(~Sepal.Length, ~Sepal.Width)
-echart(iris, ~Sepal.Length, ~Sepal.Width)
-echart(iris, ~Sepal.Length, ~Sepal.Width, series = ~Species)
-
